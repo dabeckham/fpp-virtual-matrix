8861cefc37bb2d0c06cc07422855bf9521040866
Don Beckham <dabeckham@yahoo.com>
2026-07-26 21:33:36 -0500

    Fix a broken string literal in the overlay throughput line

--- app/src/main/java/app/fppvm/tv/MainActivity.kt
+++ app/src/main/java/app/fppvm/tv/MainActivity.kt
@@ -142,10 +142,7 @@ class MainActivity : ComponentActivity() {
                             s.driftFrames, s.resyncJumps, s.syncPackets
                         )
                     )
-                    append(
-                        "%.1f fps  decode %.1f ms  paint %.1f ms
-".format(s.fps, s.decodeMs, s.paintMs)
-                    )
+                    appendLine("%.1f fps  decode %.1f ms  paint %.1f ms".format(s.fps, s.decodeMs, s.paintMs))
                     append("master ${s.multiSync.lastMaster}  rendered ${s.renderedFrames}  dropped ${s.droppedFrames}")
                     if (s.message.isNotEmpty()) append("\n${s.message}")
                 }
